af722b73247a7c9f56787a40534ada0238bf6aa3
Uzair169 <96523038+Uzair169@users.noreply.github.com>
2024-09-06 18:18:12 +0500

    Adding multi-states for generating random memes

--- src/Meme.js
+++ src/Meme.js
@@ -2,13 +2,27 @@ import React from "react"
 import memesData from "./memesData"
 
 export default function Meme() {
+    // const [memeImage, setMemeImage] = React.useState("")
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: ""
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url    //Can be const {url} = memesArray[randomNumber], this is more of a structuring
+        const url = memesArray[randomNumber].url //Can be const {url} = memesArray[randomNumber], this is more of a structuring
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
         
-    
+
     }
 
 
@@ -19,6 +33,7 @@ export default function Meme() {
                 <input type="text" className="form--input" placeholder="Bottom text" />
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
             </div>
+            <img src={meme.randomImage} className="meme--image" />
         </main>
     )
 }
